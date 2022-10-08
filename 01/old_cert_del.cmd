fcaeb782ff3014737a4983a22c34244beaf6d0de
krdondon <1571600+oklokl@users.noreply.github.com>
2022-10-09 05:59:14 +0900

    40일 지난 인증서 삭제 좀 깔끔한 버전
    https://www.codeproject.com/Questions/599352/Howplustoplusremovepluscertificateplususingpluspow
    이 버전은 매우 깔끔 하게 제거가 되네요.

---
+++ 01/old_cert_del.cmd
@@ -0,0 +1,7 @@
+@echo off
+mode con cols=15 lines=1
+REM 20221009 "https://www.codeproject.com/Questions/599352/Howplustoplusremovepluscertificateplususingpluspow"
+PowerShell -windowstyle hidden -Command "& {Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object {$_.NotAfter -lt (Get-Date).AddDays(40)} | ForEach-Object {Remove-Item -Path "Cert:\LocalMachine\Root\$($_.Thumbprint)" -Recurse -Verbose}}"
+REM PowerShell -windowstyle hidden -Command "& {Set-Location Cert: ; $expired = Get-ChildItem cert:\* ` -recurse -ExpiringInDays 0 ; $expired | Remove-Item -DeleteKey}"
+mode con cols=120 lines=30 > nul
+exit
