25d8fe62d6ca31cfa6156954775771c8d1bce85b
krdondon <1571600+oklokl@users.noreply.github.com>
2022-10-24 01:37:55 +0900

    40일 지난 인증서 삭제 좀 깔끔한 버전 & temp 폴더 비우기 추가
    40일 지난 인증서 삭제 좀 깔끔한 버전 & temp 폴더 비우기 추가

--- 02/old_cert_del.cmd
+++ 02/old_cert_del.cmd
@@ -1,5 +1,6 @@
 @echo off
 mode con cols=15 lines=1
+
 REM 20221009 "https://www.codeproject.com/Questions/599352/Howplustoplusremovepluscertificateplususingpluspow"
 PowerShell -windowstyle hidden -Command "& {Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object {$_.NotAfter -lt (Get-Date).AddDays(40)} | ForEach-Object {Remove-Item -Path "Cert:\LocalMachine\Root\$($_.Thumbprint)" -Recurse -Verbose}}"
 REM PowerShell -windowstyle hidden -Command "& {Set-Location Cert: ; $expired = Get-ChildItem cert:\* ` -recurse -ExpiringInDays 0 ; $expired | Remove-Item -DeleteKey}"
