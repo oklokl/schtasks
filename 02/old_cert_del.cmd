192e8fdff9a13ccba26babcd085ab4052c4d50a6
krdondon <1571600+oklokl@users.noreply.github.com>
2022-10-24 01:35:08 +0900

    Update old_cert_del.cmd

--- 02/old_cert_del.cmd
+++ 02/old_cert_del.cmd
@@ -3,6 +3,6 @@ mode con cols=15 lines=1
 REM 20221009 "https://www.codeproject.com/Questions/599352/Howplustoplusremovepluscertificateplususingpluspow"
 PowerShell -windowstyle hidden -Command "& {Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object {$_.NotAfter -lt (Get-Date).AddDays(40)} | ForEach-Object {Remove-Item -Path "Cert:\LocalMachine\Root\$($_.Thumbprint)" -Recurse -Verbose}}"
 REM PowerShell -windowstyle hidden -Command "& {Set-Location Cert: ; $expired = Get-ChildItem cert:\* ` -recurse -ExpiringInDays 0 ; $expired | Remove-Item -DeleteKey}"
-powershell -windowstyle hidden -Command "& {Get-ChildItem """$env:temp""" | Remove-Item -recurse; Stop-Process -name CMD}"
+powershell -windowstyle hidden -Command "& {Get-ChildItem """$env:temp""" | Remove-Item -recurse}"
 mode con cols=120 lines=30 > nul
 exit
